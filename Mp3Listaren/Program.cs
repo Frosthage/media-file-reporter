f9ca0dd958e8454f9b32846f9da0268b8dfc11e5
Mikael Frosthage <mikael.frosthage@vinngroup.se>
2020-05-16 12:51:05 +0200

    :...

---
+++ Mp3Listaren/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Mp3Listaren
+{
+    class Program
+    {
+        private static readonly Dictionary<string, string> SupportedFiles;
+
+        static Program()
+        {
+            SupportedFiles = new[]
+            {
+                // video
+                "mkv", "ogv", "avi", "wmv", "asf", "mp4", "m4p", "m4v", "mpeg", "mpg", "mpe", "mpv", "mpg", "m2v",
+                // audio
+                "aa", "aax", "aac", "aiff", "ape", "dsf", "flac", "m4a", "m4b", "m4p", "mp3", "mpc", "mpp", "ogg",
+                "oga", "wav", "wma", "wv", "webm"
+            }.ToDictionary(x => x);
+        }
+         
+        static void Main()
+        {
+            var fileInfos = Directory
+                .GetFiles(Environment.CurrentDirectory, "*", SearchOption.AllDirectories)
+                .Select(x => new FileInfo(x));
+
+            using StreamWriter streamWriter = new StreamWriter("filer.txt");
+            foreach (FileInfo fileInfo in fileInfos)
+            {
+                string extension = Path.GetExtension(fileInfo.FullName);
+                var str = fileInfo.Name[..^extension.Length];
+                
+                streamWriter.WriteLine($"{extension}\t{str}\t{fileInfo.Directory.FullName}\t{fileInfo.Length}\t{GetLength(fileInfo)}");
+            }
+        }
+
+        private static string GetLength(FileInfo fileInfo)
+        {
+            string extension = Path.GetExtension(fileInfo.FullName).TrimStart('.');
+
+            if (SupportedFiles.ContainsKey(extension))
+            {
+                using var file = TagLib.File.Create(fileInfo.FullName);
+                return file.Properties.Duration.ToString("hh\\:mm\\:ss");
+            }
+
+            return "---";
+        }
+    }
+}
---
+++ Mp3Listaren/publish.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet publish -c Release -r win10-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output .
