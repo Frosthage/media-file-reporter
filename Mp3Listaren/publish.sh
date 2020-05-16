bbef110bbfa5c21b34810f340df8f2cd975d2e1c
Mikael Frosthage <mikael.frosthage@vinngroup.se>
2020-05-16 12:56:59 +0200

    ...

--- Mp3Listaren/publish.sh
+++ Mp3Listaren/publish.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+rm Mp3Listaren.exe
+
 dotnet publish -c Release -r win10-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output .
+rm -rf bin
+rm -rf obj
+rm Mp3Listaren.pdb
+
+ 
