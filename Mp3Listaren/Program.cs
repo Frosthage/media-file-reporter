24ff3fc1ecf1807cea962ba176cb5c2f32ebc8f6
Mikael Frosthage <mikael.frosthage@vinngroup.se>
2020-06-21 22:20:16 +0200

    more stuff

--- Mp3Listaren/Program.cs
+++ Mp3Listaren/Program.cs
@@ -2,25 +2,42 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using File = TagLib.File;
 
 namespace Mp3Listaren
 {
     class Program
     {
-        private static readonly Dictionary<string, string> SupportedFiles;
+        private static readonly Dictionary<string, string> SupportedVideo;
 
+        private static readonly Dictionary<string, string> SupportedImage;
+
+        private static readonly Dictionary<string, string> SupportedAudio;
+        
         static Program()
         {
-            SupportedFiles = new[]
+            SupportedVideo = new[]
                 {
-                    // video
                     "mkv", "ogv", "avi", "wmv", "asf", "m4p", "m4v", "mpeg", "mpe", "mpv", "mpg", "m2v",
-                    // audio
-                    "aa", "aax", "aac", "aiff", "ape", "dsf", "flac", "m4a", "m4b", "m4p", "mp3", "mpc", "mpp", "ogg",
-                    "oga", "wav", "wma", "wv", "webm"
                 }
                 .Distinct()
                 .ToDictionary(x => x);
+            SupportedAudio = new[]
+            {
+                "aa", "aax", "aac", "aiff", "ape", "dsf", "flac", "m4a", "m4b", "m4p", "mp3", "mpc", "mpp", "ogg",
+                "oga", "wav", "wma", "wv", "webm"
+            }
+                .Distinct()
+                .ToDictionary(x => x);
+            
+            SupportedImage = new[]
+                {
+                    "bmp", "gif", "jpeg", "pbm", "pgm", "ppm", "pnm", "pcx", "png", "tiff", "dng", "svg"
+                }
+                .Distinct()
+                .ToDictionary(x => x);
+                
+            
         }
 
         static void Main()
@@ -36,22 +53,80 @@ namespace Mp3Listaren
             {
                 string extension = Path.GetExtension(fileInfo.FullName);
                 var str = fileInfo.Name[..^extension.Length];
+
+                var (duration, width, height, resolution, imageDate) = GetData(fileInfo);
                 
-                streamWriter.WriteLine($"{extension}\t{str}\t{fileInfo.Directory.FullName}\t{fileInfo.Length}\t{GetLength(fileInfo)}");
+                streamWriter.WriteLine($"{extension}\t" +
+                                       $"{str}\t" +
+                                       $"{fileInfo.Directory.FullName}\t" +
+                                       $"{fileInfo.Length}\t" +
+                                       $"{duration}\t", 
+                                       $"{width}\t", 
+                                       $"{height}\t", 
+                                       $"{resolution}\t", 
+                                       $"{imageDate}");
             }
         }
 
-        private static string GetLength(FileInfo fileInfo)
+        private static (string Duration, string Width, string Height, string Resolution, string ImageDate) GetData(FileInfo fileInfo)
         {
-            string extension = Path.GetExtension(fileInfo.FullName).TrimStart('.');
+            var fileType = GetFileType(fileInfo);
 
-            if (SupportedFiles.ContainsKey(extension))
+            if (fileType == FileType.Other)
             {
-                using var file = TagLib.File.Create(fileInfo.FullName);
-                return file.Properties.Duration.ToString("hh\\:mm\\:ss");
+                return ("---", "---", "---", "---", "---");
+            }
+            
+            using var file = File.Create(fileInfo.FullName);
+
+            return fileType switch
+            {
+                FileType.Image => (
+                    "---",
+                    file.Properties.PhotoWidth.ToString(),
+                    file.Properties.PhotoHeight.ToString(),
+                    $"{file.Properties.PhotoWidth}x{file.Properties.PhotoHeight}",
+                    "---"),
+                FileType.Audio => (file.Properties.Duration.ToString("hh\\:mm\\:ss"), "---", "---", "---", "---"),
+                FileType.Video => (
+                    file.Properties.Duration.ToString("hh\\:mm\\:ss"),
+                    file.Properties.VideoWidth.ToString(),
+                    file.Properties.PhotoHeight.ToString(),
+                    $"{file.Properties.VideoWidth}x{file.Properties.PhotoHeight}",
+                    "---"),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        private static FileType GetFileType(FileInfo fileInfo)
+        {
+            var extension = fileInfo.Extension.TrimStart('.');
+
+            if (SupportedAudio.ContainsKey(extension))
+            {
+                return FileType.Audio;
+            }
+            
+            if (SupportedVideo.ContainsKey(extension))
+            {
+                return FileType.Video;
+            }
+            
+            if (SupportedImage.ContainsKey(extension))
+            {
+                return FileType.Image;
             }
 
-            return "---";
+            return FileType.Other;
         }
+        
+    }
+
+    internal enum FileType
+    {
+        Image,
+        Audio,
+        Video,
+        Other
     }
 }
