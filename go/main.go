2658fe48313dc55c34814a10228d5df36498ce66
Mikael Frosthage <mikael.frosthage@vinngroup.se>
2020-07-23 23:12:18 +0200

    kjnlödsfgjklhbfdgs jhklgfds

--- go/main.go
+++ go/main.go
@@ -2,14 +2,99 @@ package main
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"frosthage.com/mp3-listaren/formats"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 )
 
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
+	errc := make(chan error, 1)
+	go func() {
+		// Close the paths channel after Walk returns.
+		defer close(paths)
+		// No select needed for this send, since errc is buffered.
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.Mode().IsRegular() {
+				return nil
+			}
+			select {
+			case paths <- path:
+			case <-done:
+				return errors.New("walk canceled")
+			}
+			return nil
+		})
+	}()
+	return paths, errc
+}
+
+type result struct {
+	media formats.Media
+	err   error
+}
+
+func digester(done <-chan struct{}, paths <-chan string, c chan<- result) {
+	for path := range paths {
+
+		fileInfo, err := os.Stat(path)
+		var media formats.Media
+
+		if err == nil {
+			media = formats.CreateMedia(path, fileInfo)
+		}
+
+		select {
+		case c <- result{media, err}:
+		case <-done:
+			return
+		}
+	}
+}
+
+
 func main() {
+
+	done := make(<-chan struct{})
+	paths, _ := walkFiles(done, ".")
+
+	// Start a fixed number of goroutines to read and digest files.
+	c := make(chan result)
+	var wg sync.WaitGroup
+	numDigesters := runtime.NumCPU()
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, paths, c)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	writer := csv.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	for r := range c {
+		if r.err == nil {
+			r, _ := r.media.GetRecord()
+			writer.Write(strings.Split(r, "\t"))
+		}
+	}
+}
+
+
+func main2() {
 
 	var files []formats.Media
 
@@ -63,3 +148,4 @@ func main() {
 		writer.Write(strings.Split(record, "\t"))
 	}
 }
+
